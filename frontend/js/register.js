d02aa9c11f030f3a7c7c6ef75ab71d4ff399deb3
PrincessKikary <aishakikari@gmail.com>
2025-04-18 10:32:25 +0300

    updates

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -37,4 +37,9 @@ document.addEventListener("DOMContentLoaded", function () {
             alert("⚠️ An error occurred while registering.");
         }
     });
-});
+
+    document.querySelector('.hamburger').addEventListener('click', function() {
+        document.body.classList.toggle('nav-open');
+    });
+}
+);
